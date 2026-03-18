f49b706251ce16a8785c6f2b795df045b443797a
pramms19 <pramadashrestha17@gmail.com>
2026-03-18 11:15:52 +0545

    Added react-hook-form and zod to Login.

--- src/components/SignInCard.tsx
+++ src/components/SignInCard.tsx
@@ -6,8 +6,60 @@ import { Input } from "./ui/input";
 import { InputGroup, InputGroupAddon } from "./ui/input-group";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { NavLink } from "react-router-dom";
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
+
+const LoginSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(5, "Password must be at least 5 characters"),
+  terms: z
+    .boolean()
+    .refine((val) => val === true, "You must accept terms & conditions"),
+});
+
+type LoginFormData = z.infer<typeof LoginSchema>;
 
 export default function SignInCard() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const { register, handleSubmit, control, formState } = useForm<LoginFormData>(
+    {
+      resolver: zodResolver(LoginSchema),
+    },
+  );
+
+  const onSubmit = async (formData: LoginFormData) => {
+    setServerError(null);
+    const options = {
+      method: "POST",
+      url: "https://api.freeapi.app/api/v1/users/login",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      data: {
+        ...formData,
+        role: "USER", // The API requires a role
+      },
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      toast.success("Logged In Successfully!", { position: "top-center" });
+      console.log("Success:", data);
+    } catch (error: any) {
+      console.error("Error:", error);
+      setServerError(error.response?.data?.message || "Something went wrong!");
+    }
+  };
+  console.log(formState.isSubmitting);
+
   return (
     <div className="px-4 sm:px-6 lg:px-8 pt-14 flex justify-center items-center">
       <Card className="bg-white max-w-sm md:max-w-md min-w-xs md:min-w-sm shadow-sm py-6">
@@ -15,29 +67,58 @@ export default function SignInCard() {
           Sign In
         </CardTitle>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <FieldGroup>
               <Field>
                 <Input
+                  {...register("email")}
                   placeholder="Email"
                   required
                   className="border rounded-sm text-xs md:text-sm"
                 />
               </Field>
+              {formState.errors?.email?.message && (
+                <p className="text-red-700 mb-4">
+                  {formState.errors.email.message}
+                </p>
+              )}
+
               <Field>
                 <InputGroup>
-                  <Input placeholder="Password" required className="text-xs md:text-sm"/>
-                  <InputGroupAddon align="inline-end">
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    {...register("password")}
+                    placeholder="Password"
+                    required
+                    className="text-xs md:text-sm"
+                  />
+                  <InputGroupAddon
+                    align="inline-end"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
                     <Eye />
                   </InputGroupAddon>
                 </InputGroup>
               </Field>
+              {formState.errors?.password?.message && (
+                <p className="text-red-700 mb-4">
+                  {formState.errors.password.message}
+                </p>
+              )}
 
               <div className="flex justify-between">
                 <div className="flex gap-1">
-                  <Checkbox
-                    id="terms-checkbox-basic"
-                    name="terms-checkbox-basic"
+                  <Controller
+                    name="terms"
+                    control={control}
+                    render={({ field }) => (
+                      <Checkbox
+                        id="terms"
+                        name="terms-checkbox-basic"
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    )}
                   />
                   <div className="text-xs md:text-sm text-neutral-400 font-normal">
                     Remember me
@@ -49,9 +130,20 @@ export default function SignInCard() {
                 </div>
               </div>
 
+              {serverError && (
+                <p className="text-red-600 text-center mb-4 text-xs md:text-sm">
+                  {serverError}
+                </p>
+              )}
+
               <Field orientation="horizontal">
-                <Button className="w-full rounded-full bg-primary text-xs md:text-sm lg:text-base font-medium text-white hover:bg-secondary-foreground">
-                  Login
+                <Button
+                  //   onClick={handleSubmit(onSubmit)}
+                  type="submit"
+                  disabled={formState.isSubmitting}
+                  className="w-full rounded-full bg-primary text-xs md:text-sm lg:text-base font-medium text-white hover:bg-secondary-foreground"
+                >
+                  {formState.isSubmitting ? "Logging in..." : "Login"}
                 </Button>
               </Field>
               <div className="flex gap-1 justify-center text-xs md:text-sm">
--- src/components/SignUpCard.tsx
+++ src/components/SignUpCard.tsx
@@ -7,33 +7,45 @@ import { InputGroup, InputGroupAddon } from "./ui/input-group";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { NavLink } from "react-router-dom";
 import * as z from "zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "sonner";
 
-const UserSchema = z.object({
-  email: z.email("Invalid email"),
-  username: z.string().min(5, "Username must be at least 5 characters"),
-  password: z.string().min(5, "Password must be at least 5 characters"),
-});
+const UserSchema = z
+  .object({
+    email: z.string().email("Invalid email"),
+    username: z.string().min(5, "Username must be at least 5 characters"),
+    password: z.string().min(5, "Password must be at least 5 characters"),
+    confirmPassword: z.string(),
+    terms: z
+      .boolean()
+      .refine((val) => val === true, "You must accept terms & conditions"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 type UserFormData = z.infer<typeof UserSchema>;
 
 export default function SignUpCard() {
+  const [showPassword, setShowPassword] = useState(false);
+
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const {
-    register,
-    handleSubmit,
-    formState
-  } = useForm<UserFormData>({
+  const { register, handleSubmit, control, formState } = useForm<UserFormData>({
     resolver: zodResolver(UserSchema),
+    defaultValues: {
+      terms: false,
+    },
   });
 
   const onSubmit = async (formData: UserFormData) => {
     setServerError(null);
+    // remove unnecessary fields
+    const { confirmPassword, terms, ...cleanData } = formData;
     const options = {
       method: "POST",
       url: "https://api.freeapi.app/api/v1/users/register",
@@ -42,7 +54,7 @@ export default function SignUpCard() {
         "content-type": "application/json",
       },
       data: {
-        ...formData,
+        ...cleanData,
         // email: formData.email,
         // password: formData.password,
         // username: formData.username,
@@ -52,14 +64,17 @@ export default function SignUpCard() {
 
     try {
       const { data } = await axios.request(options);
-      toast.success("Account Created Successfully!",{ position: "top-center" });
+      toast.success("Account Created Successfully!", {
+        position: "top-center",
+      });
       console.log("Success:", data);
     } catch (error: any) {
       console.error("Error:", error);
       setServerError(error.response?.data?.message || "Something went wrong!");
     }
   };
-console.log( formState.isSubmitting)
+  console.log(formState.isSubmitting);
+
   return (
     <div className="px-4 sm:px-6 lg:px-8 pt-14 flex justify-center items-center">
       <Card className="bg-white max-w-sm md:max-w-md min-w-xs md:min-w-sm shadow-sm py-6">
@@ -78,7 +93,9 @@ console.log( formState.isSubmitting)
                 />
               </Field>
               {formState.errors?.email?.message && (
-                <p className="text-red-700 mb-4">{formState.errors.email.message}</p>
+                <p className="text-red-700 mb-4">
+                  {formState.errors.email.message}
+                </p>
               )}
               <Field>
                 <Input
@@ -89,30 +106,36 @@ console.log( formState.isSubmitting)
                 />
               </Field>
               {formState.errors?.username?.message && (
-                <p className="text-red-700 mb-4">{formState.errors.username.message}</p>
+                <p className="text-red-700 mb-4">
+                  {formState.errors.username.message}
+                </p>
               )}
 
               <Field>
                 <InputGroup>
                   <Input
+                  type={showPassword? "text":"password"}
                     {...register("password")}
                     placeholder="Password"
                     required
                     className="text-xs md:text-sm"
                   />
-                  <InputGroupAddon align="inline-end">
+                  <InputGroupAddon align="inline-end" onClick={()=> setShowPassword(prev=> !prev)}>
                     <Eye />
                   </InputGroupAddon>
                 </InputGroup>
               </Field>
               {formState.errors?.password?.message && (
-                <p className="text-red-700 mb-4">{formState.errors.password.message}</p>
+                <p className="text-red-700 mb-4">
+                  {formState.errors.password.message}
+                </p>
               )}
 
               <Field>
                 <InputGroup>
                   <Input
-                    {...register("password")}
+                    {...register("confirmPassword")}
+                    type="password"
                     placeholder="Confirm Password"
                     required
                     className="text-xs md:text-sm"
@@ -122,18 +145,29 @@ console.log( formState.isSubmitting)
                   </InputGroupAddon>
                 </InputGroup>
               </Field>
-              {formState.errors?.password?.message && (
-                <p className="text-red-700 mb-4">{formState.errors.password.message}</p>
+              {formState.errors?.confirmPassword?.message && (
+                <p className="text-red-700 mb-4">
+                  {formState.errors.confirmPassword.message}
+                </p>
               )}
 
               <div className="flex gap-1">
-                <Checkbox
-                  id="terms-checkbox-basic"
-                  name="terms-checkbox-basic"
+                <Controller
+                  name="terms"
+                  control={control}
+                  render={({ field }) => (
+                    <Checkbox
+                      id="terms"
+                      name="terms-checkbox-basic"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  )}
                 />
-                <div className="text-xs md:text-sm text-neutral-400 font-normal">
+
+                <label className="text-xs md:text-sm text-neutral-400 font-normal">
                   Accept all Terms & Conditions
-                </div>
+                </label>
               </div>
 
               {serverError && (
@@ -144,11 +178,12 @@ console.log( formState.isSubmitting)
 
               <Field orientation="horizontal">
                 <Button
-                  onClick={handleSubmit(onSubmit)}
+                //   onClick={handleSubmit(onSubmit)}
                   type="submit"
+                   disabled={formState.isSubmitting}
                   className="w-full rounded-full bg-primary text-xs md:text-sm lg:text-base font-medium text-white hover:bg-secondary-foreground"
                 >
-                  Create Account
+                  {formState.isSubmitting ? "Creating..." : "Create Account"}
                 </Button>
               </Field>
               <div className="flex gap-1 justify-center text-xs md:text-sm">
